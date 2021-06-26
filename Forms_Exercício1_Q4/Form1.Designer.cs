ecdb86b5a5eee95c35b9d81dca0c682b8da8ed08
jessicatrettin <jessica.trettin@gmail.com>
2021-06-25 22:09:41 -0300

    aula 25/06

---
+++ Forms_Exercício1_Q4/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace Forms_Exercício1_Q4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumero4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNumero3 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(30, 48);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(125, 27);
+            this.txtNumero1.TabIndex = 0;
+            this.txtNumero1.TextChanged += new System.EventHandler(this.txtNumero1_TextChanged);
+            this.txtNumero1.Leave += new System.EventHandler(this.txtNumero1_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Número 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Número 2";
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(30, 108);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(125, 27);
+            this.txtNumero2.TabIndex = 2;
+            this.txtNumero2.TextChanged += new System.EventHandler(this.txtNumero2_TextChanged);
+            this.txtNumero2.Leave += new System.EventHandler(this.txtNumero2_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Número 4";
+            // 
+            // txtNumero4
+            // 
+            this.txtNumero4.Location = new System.Drawing.Point(30, 228);
+            this.txtNumero4.Name = "txtNumero4";
+            this.txtNumero4.Size = new System.Drawing.Size(125, 27);
+            this.txtNumero4.TabIndex = 6;
+            this.txtNumero4.TextChanged += new System.EventHandler(this.txtNumero4_TextChanged);
+            this.txtNumero4.Leave += new System.EventHandler(this.txtNumero4_Leave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Número 3";
+            // 
+            // txtNumero3
+            // 
+            this.txtNumero3.Location = new System.Drawing.Point(30, 168);
+            this.txtNumero3.Name = "txtNumero3";
+            this.txtNumero3.Size = new System.Drawing.Size(125, 27);
+            this.txtNumero3.TabIndex = 4;
+            this.txtNumero3.TextChanged += new System.EventHandler(this.txtNumero3_TextChanged);
+            this.txtNumero3.Leave += new System.EventHandler(this.txtNumero3_Leave);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            ""});
+            this.listBox1.Location = new System.Drawing.Point(332, 46);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(193, 164);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.Visible = false;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBoxAdicao);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Items.AddRange(new object[] {
+            ""});
+            this.listBox2.Location = new System.Drawing.Point(546, 46);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(193, 164);
+            this.listBox2.TabIndex = 9;
+            this.listBox2.Visible = false;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.ListBoxMultiplicacao);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 29);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Adição";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnAdicaoo);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(180, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 29);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Multiplicação";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnMultiplicacao);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(180, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 29);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Zerar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnZerar);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNumero4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNumero3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumero1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumero4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNumero3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Forms_Exercício1_Q4/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms_Exercício1_Q4
+{
+    public partial class Form1 : Form
+    {
+        int num1, num2, num3, num4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNumero1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void txtNumero2_TextChanged(object sender, EventArgs e)
+        {
+
+        }   
+
+        private void txtNumero3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtNumero1_Leave(object sender, EventArgs e)
+        {
+            num1 = int.Parse(txtNumero1.Text);
+        }
+
+        private void txtNumero2_Leave(object sender, EventArgs e)
+        {
+            num2 = int.Parse(txtNumero2.Text);
+        }
+
+        private void txtNumero3_Leave(object sender, EventArgs e)
+        {
+            num3 = int.Parse(txtNumero3.Text);
+        }
+
+        private void txtNumero4_Leave(object sender, EventArgs e)
+        {
+            num4 = int.Parse(txtNumero4.Text);
+        }
+
+        private void btnAdicaoo(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count == 1)
+            {
+                listBox1.Items.Add("ADIÇÃO");
+                listBox1.Items.Add($"{num1} + {num2} = {num1 + num2}");
+                listBox1.Items.Add($"{num1} + {num3} = {num1 + num3}");
+                listBox1.Items.Add($"{num1} + {num4} = {num1 + num4}");
+                listBox1.Items.Add($"{num2} + {num3} = {num2 + num3}");
+                listBox1.Items.Add($"{num2} + {num4} = {num2 + num4}");
+                listBox1.Items.Add($"{num3} + {num4} = {num3 + num4}");
+            }
+            listBox1.Visible = true;
+        }
+
+        private void btnMultiplicacao(object sender, EventArgs e)
+        {
+            if (listBox2.Items.Count == 1)
+            {
+                listBox2.Items.Add("MULTIPLICAÇÃO");
+                listBox2.Items.Add($"{num1} x {num2} = {num1 * num2}");
+                listBox2.Items.Add($"{num1} x {num3} = {num1 * num3}");
+                listBox2.Items.Add($"{num1} x {num4} = {num1 * num4}");
+                listBox2.Items.Add($"{num2} x {num3} = {num2 * num3}");
+                listBox2.Items.Add($"{num2} x {num4} = {num2 * num4}");
+                listBox2.Items.Add($"{num3} x {num4} = {num3 * num4}");
+            }
+            listBox2.Visible = true;
+        }
+
+        private void btnZerar(object sender, EventArgs e)
+        {
+            txtNumero1.Clear();
+            txtNumero2.Clear();
+            txtNumero3.Clear();
+            txtNumero4.Clear();
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+
+        private void txtNumero4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBoxAdicao(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void ListBoxMultiplicacao(object sender, EventArgs e)
+        {
+ 
+        }
+    }
+}
